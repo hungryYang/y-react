c658f51da098ce059305d0422e5b5951c2b75d7b
laoyang <yangchengrun@kingsoft.com>
2021-06-21 15:32:17 +0800

    架子搭建

---
+++ build/webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+    mode: 'development',
+    devtool: 'source-map',
+    entry: path.resolve(__dirname, '../lib/index.tsx'),
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, '../dist')
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'y-react fuck yeah',
+            template: path.resolve(__dirname, '../index.html')
+        }),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                loader: 'awesome-typescript-loader',
+            },
+        ]
+    },
+    devServer: {
+        contentBase: path.resolve(__dirname, '../dist'),
+    },
+}
